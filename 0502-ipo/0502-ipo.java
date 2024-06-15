f987fa14c535b8eac0d66269934a3efda086e34b
Ramdeen Nishad <88136985+ramdeennishad89@users.noreply.github.com>
2024-06-15 12:42:53 +0530

    Time: 92 ms (24.51%), Space: 62.8 MB (24.84%) - LeetHub

---
+++ 0502-ipo/0502-ipo.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        PriorityQueue<int[]> minC= new PriorityQueue<>((a,b) -> a[0] - b[0]);
+        PriorityQueue<Integer> maxP= new PriorityQueue<>((a,b) -> b - a);
+        
+        for(int i=0; i<profits.length; i++){
+            minC.offer(new int[] { capital[i], profits[i]});
+        }
+        while( k-- > 0){
+            while(!minC.isEmpty() && minC.peek()[0] <= w){
+                maxP.add(minC.poll()[1]);
+            }
+            if(maxP.isEmpty()){
+                break;
+            }
+            w+=maxP.poll();
+        }
+        return w;
+    }
+}
