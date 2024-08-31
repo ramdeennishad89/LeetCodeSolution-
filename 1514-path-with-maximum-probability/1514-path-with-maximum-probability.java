db89a7ef2db55bed4ff38980ac2aafdc0508de47
Ramdeen Nishad <88136985+ramdeennishad89@users.noreply.github.com>
2024-08-31 05:41:22 +0530

    Time: 10 ms (92.38%), Space: 55.9 MB (85.72%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.java
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.java
@@ -1,42 +1,27 @@
 class Solution {
-    public double maxProbability(int n, int[][] edges, double[] succProb, int start, int end)  {
-    // {a: [(b, probability_ab)]}
-    List<Pair<Integer, Double>>[] graph = new List[n];
-    // (the probability to reach u, u)
-    Queue<Pair<Double, Integer>> maxHeap =
-        new PriorityQueue<>((a, b) -> Double.compare(b.getKey(), a.getKey()));
+    public double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
+        double[] maxProb = new double[n];
+        maxProb[start_node] = 1.0;
 
-    maxHeap.offer(new Pair<>(1.0, start));
-    boolean[] seen = new boolean[n];
+        for (int i = 0; i < n - 1; i++) {
+            boolean updated = false;
+            for (int j = 0; j < edges.length; j++) {
+                int u = edges[j][0];
+                int v = edges[j][1];
+                double prob = succProb[j];
 
-    for (int i = 0; i < n; ++i)
-      graph[i] = new ArrayList<>();
+                if (maxProb[u] * prob > maxProb[v]) {
+                    maxProb[v] = maxProb[u] * prob;
+                    updated = true;
+                }
+                if (maxProb[v] * prob > maxProb[u]) {
+                    maxProb[u] = maxProb[v] * prob;
+                    updated = true;
+                }
+            }
+            if (!updated) break;
+        }
 
-    for (int i = 0; i < edges.length; ++i) {
-      final int u = edges[i][0];
-      final int v = edges[i][1];
-      final double prob = succProb[i];
-      graph[u].add(new Pair<>(v, prob));
-      graph[v].add(new Pair<>(u, prob));
-    }
-
-    while (!maxHeap.isEmpty()) {
-      final double prob = maxHeap.peek().getKey();
-      final int u = maxHeap.poll().getValue();
-      if (u == end)
-        return prob;
-      if (seen[u])
-        continue;
-      seen[u] = true;
-      for (Pair<Integer, Double> node : graph[u]) {
-        final int nextNode = node.getKey();
-        final double edgeProb = node.getValue();
-        if (seen[nextNode])
-          continue;
-        maxHeap.add(new Pair<>(prob * edgeProb, nextNode));
-      }
-    }
-
-    return 0;
+        return maxProb[end_node];
     }
 }
