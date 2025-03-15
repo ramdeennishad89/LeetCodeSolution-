ef0f6e074509a5cfbd045adba49a3b0f56e778c6
Ramdeen Nishad <88136985+ramdeennishad89@users.noreply.github.com>
2025-03-15 16:07:24 +0530

    Time: 15 ms (100%), Space: 56.3 MB (55.72%) - LeetHub

--- 2560-house-robber-iv/2560-house-robber-iv.java
+++ 2560-house-robber-iv/2560-house-robber-iv.java
@@ -1,28 +1,30 @@
 class Solution {
-    public int minCapability(int[] nums, int k) {
-       int left = 0, right = 0;
-        for (int num : nums) {
-            right = Math.max(right, num);
-        }
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (canRob(nums, k, mid)) {
-                right = mid;
-            } else {
-                left = mid + 1;
-            }
-        }
-        return left;
-    }
+    public int minCapability(int[] a, int k) {
+        int n = a.length;
+        int lo = Integer.MAX_VALUE;
 
-    private boolean canRob(int[] nums, int k, int maxCap) {
-        int count = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] <= maxCap) {
-                count++;
-                i++; // Skip the next house
+        for(int  x: a) lo = Math.min(lo, x);
+        int hi = 0;
+        for(int x: a) hi = Math.max(hi, x);
+
+        int ans = 0;
+        while(hi >= lo){
+            int mid = (lo + hi) / 2;
+            int max = 0;
+
+            for(int i = 0; i < n ;i++){
+                if(a[i] <= mid){
+                    i++;
+                    max++;
+                }
             }
+
+            if(max >= k){
+                hi = mid - 1;
+                ans = mid;
+            }
+            else lo = mid + 1;
         }
-        return count >= k; 
+        return ans;       
     }
 }
