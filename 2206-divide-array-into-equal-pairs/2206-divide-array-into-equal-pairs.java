6d8e6afac5075179be9030c62d7ab704dc507fe6
Ramdeen Nishad <88136985+ramdeennishad89@users.noreply.github.com>
2025-03-17 15:46:52 +0530

    Time: 8 ms (40.3%), Space: 44.7 MB (39.07%) - LeetHub

---
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean divideArray(int[] nums) {
+        HashMap<Integer, Integer> countMap = new HashMap<>();
+        for (int num : nums) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+        }
+        for (int count : countMap.values()) {
+            if (count % 2 != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
