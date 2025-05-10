0dccc1d48b41490b56dd746c0b55ed557f465045
Ramdeen Nishad <88136985+ramdeennishad89@users.noreply.github.com>
2025-05-10 12:26:04 +0530

    Time: 3 ms (88.73%), Space: 56.5 MB (100%) - LeetHub

--- 2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros/2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros.java
+++ 2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros/2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros.java
@@ -1,28 +1,29 @@
 class Solution {
-    public int minSum(int[] nums1, int[] nums2) {
-        int sum1 = 0, sum2 = 0;
-        int zero1 = 0, zero2 = 0;
+    public long minSum(int[] nums1, int[] nums2) {
+        long[] result1 = calculateSumAndZeroCount(nums1);
+        long[] result2 = calculateSumAndZeroCount(nums2);
+        
+        long total1 = result1[0] + result1[1];
+        long total2 = result2[0] + result2[1];
+        
+        if (total1 == total2) {
+            return total1;
+        }
+        
+        long[] smallerResult = total1 < total2 ? result1 : result2;
+        long[] largerResult = (smallerResult == result1 ? result2 : result1);
+        
+        return smallerResult[1] == 0 ? -1 : largerResult[0] + largerResult[1];
+    }
 
-        for (int num : nums1) {
-            sum1 += num;
+    private long[] calculateSumAndZeroCount(int[] nums) {
+        long[] sumAndZeroCount = {0, 0}; // [sum, zeroCount]
+        for (int num : nums) {
             if (num == 0) {
-                sum1 += 1;
-                zero1++;
+                sumAndZeroCount[1]++;
             }
+            sumAndZeroCount[0] += num;
         }
-
-        for (int num : nums2) {
-            sum2 += num;
-            if (num == 0) {
-                sum2 += 1;
-                zero2++;
-            }
-        }
-
-        if ((zero1 == 0 && sum2 > sum1) || (zero2 == 0 && sum1 > sum2)) {
-            return -1;
-        }
-
-        return Math.max(sum1, sum2);
+        return sumAndZeroCount;
     }
 }
